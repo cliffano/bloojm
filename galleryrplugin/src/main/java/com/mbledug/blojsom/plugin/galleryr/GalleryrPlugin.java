160b07aa9898b3de5a203ba385b7355c65ce5951
cliffano <cliffano@gmail.com>
2006-11-06 13:43:53 +0000

    Added project skeleton for GalleryrPlugin.

---
+++ galleryrplugin/src/main/java/com/mbledug/blojsom/plugin/galleryr/GalleryrPlugin.java
@@ -0,0 +1,36 @@
+package com.mbledug.blojsom.plugin.galleryr;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.blojsom.blog.Blog;
+import org.blojsom.blog.Entry;
+import org.blojsom.plugin.Plugin;
+import org.blojsom.plugin.PluginException;
+
+public class GalleryrPlugin implements Plugin {
+
+	public void cleanup() throws PluginException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void destroy() throws PluginException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init() throws PluginException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Entry[] process(HttpServletRequest arg0, HttpServletResponse arg1,
+			Blog arg2, Map arg3, Entry[] arg4) throws PluginException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
